9b8b09538d65af8a07bb2b310410d8a940d06f4a
Sergey <smyasnikov@gmail.com>
2022-03-02 14:17:25 +0000

    day 2 final

--- roles/helloworld/molecule/default/tests/test_default.rb
+++ roles/helloworld/molecule/default/tests/test_default.rb
@@ -1,3 +1,9 @@
 describe user('root') do
   it { should exist }
 end
+
+describe file('/tmp/hello.txt') do
+  it { should exist }
+  it { should be_file }
+  its('content') { should match(%r{Hello world!}) }
+end
